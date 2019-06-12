80922654d3aab9d5e27b138ee57f48527dee7296
Anthony Ng <newyork.anthonyng@gmail.com>
2019-06-12 19:35:18 -0400

    gh-pages deploy

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
 
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname),
     filename: "[name].js"
   },
 
