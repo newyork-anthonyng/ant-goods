2d9f7575eea1ef70ee96927142e5f72b23766995
Anthony Ng <newyork.anthonyng@gmail.com>
2019-06-12 17:20:32 -0400

    Initial commit 🔥

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { h, render, Component } from "preact";
+
+const App = () => <h1>Hello World</h1>;
+
+render(<App />, document.body);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.js"),
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlPlugin()
+  ]
+};
